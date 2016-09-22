fc5b102a13bbe9c9eacb8b93774458f232ac7dcb
PavoReal <Garrison.peacock@gmail.com>
2016-09-21 17:50:43 -0700

    Added a simple watch dog static class

---
+++ Bulletproof/src/main/java/us/newberg/bulletproof/WatchDog.java
@@ -0,0 +1,51 @@
+package us.newberg.bulletproof;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * FTC team 6712 Bulletproof
+ */
+public class WatchDog
+{
+    private static final AtomicBoolean _shouldStop = new AtomicBoolean(false);
+
+    private static void Reset()
+    {
+        _shouldStop.set(false);
+    }
+
+    // TODO(Garrison): Test this...
+    public static void Watch(final Runnable task, final long millis) throws InterruptedException
+    {
+        Thread worker = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    Thread.sleep(millis);
+                } catch (InterruptedException e)
+                {
+                    // TODO(Garrison): Error handling
+                    e.printStackTrace();
+                }
+
+                if (!_shouldStop.get())
+                {
+                    task.run();
+                }
+            }
+        });
+
+        worker.start();
+        worker.join();
+
+        Reset();
+    }
+
+    public static void Stop()
+    {
+        _shouldStop.set(true);
+    }
+}
